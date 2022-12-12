dca97a9b39ca5f742d809d1392088ab293867c4c
A199T <118428870+A199T@users.noreply.github.com>
2022-12-12 08:08:12 +0530

    Add files via upload

--- shell.sh
+++ shell.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "Plenty of disk available"
+
+echo "plenty of disk available"
 
 echo "There could be a problem in the near future"
 
-echo "Maybe we should look at clearing out old files"
+echo "Maybe we should look at clearing old files"
 
 echo "We could have a serious problem on our hands soon"
 
 echo "Something is not quite right here"
+
+echo "5$"
